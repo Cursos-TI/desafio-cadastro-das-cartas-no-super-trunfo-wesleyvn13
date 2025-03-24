60abe2e845dda4515ab5980380b359f3356d7b89
Wesleynicodemos <202501691871@alunos.estacio.br>
2025-03-24 11:37:08 -0300

    Adiçao de menu
    Adicao de menu para o usuario escolher qual atributo ele quer comparar

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@ int main() {
     char estado, estado2; // Armazena um único caractere ex "A".
     char codigo_da_carta[5], codigo_da_carta2[5]; // Armazena o codigo da carta ex "A01".
     char nomeCidade[15], nomeCidade2[15]; // Armazena o nome daS cidadeS.
-    int populacao, populacao2, numero_de_pontos_turisticos, numero_de_pontos_turisticos2; // Armazena o numero da população e quantidade de pontos turisticos.
+    int populacao, populacao2, numero_de_pontos_turisticos, numero_de_pontos_turisticos2, opcao; // Armazena o numero da população e quantidade de pontos turisticos.
     float area, area2, pib, pib2, pibPerCapita, pibPerCapita2, densidadePopulacional, densidadePopulacional2; // Armazena a area da cidade, pib, pib per capita e densidade populacional.
     float superPoder, superPoder2;
 
@@ -95,12 +95,88 @@ superPoder2 = (float)populacao2 + area2 + (float)numero_de_pontos_turisticos2 + 
 
 // Mostra o resultado da comparação para o Usuario.
 // Aqui a comparação é feita diretamente no codigo sem a necessidade de perguntar ao usuario.
+printf("Escolha um indicador para comparar: \n");
+printf("1 - Populacao.\n");
+printf("2 - area.\n");
+printf("3 - PIB.\n");
+printf("4 - Pontos Turisticos.\n");
+printf("5 - Dencidade Populacional.\n");
+printf("6 - PIB PerCapta.\n");
+scanf("%d", &opcao);
 
+switch (opcao)
+{
+case 1:
 if (populacao > populacao2) {
+    printf("a populacao da cidade 1: %s e de %d\n", nomeCidade, populacao);
+    printf("a populacao da cidade 2: %s e de %d\n", nomeCidade2, populacao2);
+    printf("a carta 1 ( %s ) venceu!\n", nomeCidade);
+} else {
+    printf("a populacao da cidade 1: %s e de %d\n", nomeCidade, populacao);
+    printf("a populacao da cidade 2: %s e de %d\n", nomeCidade2, populacao2);
+    printf("a carta 2 ( %s ) venceu!\n", nomeCidade2);
+};
+    break;
+case 2:
+if (area > area2) {
+    printf("a area da cidade 1: %s e de %f\n", nomeCidade, area);
+    printf("a area da cidade 2: %s e de %f\n", nomeCidade2, area2);
     printf("a carta 1 ( %s ) venceu!", nomeCidade);
 } else {
-    printf("a carta 2 ( %s ) venceu!", nomeCidade2);
+    printf("a area da cidade 1: %s e de %f\n", nomeCidade, area);
+    printf("a area da cidade 2: %s e de %f\n", nomeCidade2, area2);
+    printf("a carta 2 ( %s ) venceu!\n", nomeCidade2);
 };
+    break;
+case 3:
+if (pib > pib2) {
+    printf("O pib da cidade 1: %s e de %f\n", nomeCidade, pib);
+    printf("o pib da cidade 2: %s e de %f\n", nomeCidade2, pib2);
+    printf("a carta 1 ( %s ) venceu!\n", nomeCidade);
+} else {
+    printf("a area da cidade 1: %s e de %f\n", nomeCidade, pib);
+    printf("a area da cidade 2: %s e de %f\n", nomeCidade2, pib2);
+    printf("a carta 2 ( %s ) venceu!\n", nomeCidade2);
+};
+    break;
+case 4:
+if (numero_de_pontos_turisticos > numero_de_pontos_turisticos2) {
+    printf("O numero de P. turisticos da cidade 1: %s e de %d\n", nomeCidade, numero_de_pontos_turisticos);
+    printf("O numero de P. turisticos 2: %s e de %d\n", nomeCidade2, numero_de_pontos_turisticos2);
+    printf("a carta 1 ( %s ) venceu!\n", nomeCidade);
+} else {
+    printf("O numero de P. turisticos da cidade 1: %s e de %f\n", nomeCidade, numero_de_pontos_turisticos);
+    printf("O numero de P. turisticos da cidade 2: %s e de %f\n", nomeCidade2, numero_de_pontos_turisticos2);
+    printf("a carta 2 ( %s ) venceu!\n", nomeCidade2);
+};
+    break;
+    case 5:
+if (densidadePopulacional < densidadePopulacional2) {
+    printf("A densidade pupilacional da cidade 1: %s e de %d\n", nomeCidade, densidadePopulacional);
+    printf("A densidade pupilacional  2: %s e de %d\n", nomeCidade2, densidadePopulacional2);
+    printf("a carta 1 ( %s ) venceu!\n", nomeCidade);
+} else {
+    printf("A densidade pupilacional da cidade 1: %s e de %f\n", nomeCidade, densidadePopulacional);
+    printf("A densidade pupilacional da cidade 2: %s e de %f\n", nomeCidade2, densidadePopulacional2);
+    printf("a carta 2 ( %s ) venceu!\n", nomeCidade2);
+};
+    break;
+    case 6:
+if (pibPerCapita > pibPerCapita2) {
+    printf("O PIB per Capita da cidade 1: %s e de %d\n", nomeCidade, pibPerCapita);
+    printf("O PIB per Capita 2: %s e de %d\n", nomeCidade2, pibPerCapita2);
+    printf("a carta 1 ( %s ) venceu!\n", nomeCidade);
+} else {
+    printf("O PIB per Capita da cidade 1: %s e de %f\n", nomeCidade, pibPerCapita);
+    printf("O PIB per Capita da cidade 2: %s e de %f\n", nomeCidade2, pibPerCapita2);
+    printf("a carta 2 ( %s ) venceu!\n", nomeCidade2);
+};
+    break;
+
+default:
+    break;
+}
+
 
      
     return 0; // Indica que o programa foi executado com sucesso.
